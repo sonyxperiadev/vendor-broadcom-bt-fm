cbf8b3e4935be3be1343c9f690b7bc7385bde773
Alin Jerpelea <alin.jerpelea@sonymobile.com>
2017-07-01 07:01:17 +0200

    update default sysfs path
    set default path to new kernel sysfs and make 3.10 kernel path the exception
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sonymobile.com>

--- brcm-uim-sysfs/Android.mk
+++ brcm-uim-sysfs/Android.mk
@@ -18,9 +18,9 @@ LOCAL_CLANG := false
 LOCAL_CFLAGS:= -c -W -Wall -O2 -D_POSIX_SOURCE -DUIM_DEBUG -DBLUEDROID_ENABLE_V4L2
 LOCAL_SHARED_LIBRARIES:= libnetutils libcutils liblog
 
-SYSFS_PREFIX := "/sys/bus/platform/drivers/bcm_ldisc/bcmbt_ldisc.93"
-ifeq ($(TARGET_KERNEL_VERSION),3.18)
 SYSFS_PREFIX := "/sys/bus/platform/drivers/bcm_ldisc/soc:bcmbt_ldisc"
+ifeq ($(TARGET_KERNEL_VERSION),3.10)
+SYSFS_PREFIX := "/sys/bus/platform/drivers/bcm_ldisc/bcmbt_ldisc.93"
 endif
 
 LOCAL_CFLAGS += -DSYSFS_PREFIX=\"$(SYSFS_PREFIX)\"
